949c5346bcdca87232a6028ba0c99f1b1f7875d6
kyliemks <ksm.kylie@gmail.com>
2016-11-10 21:21:06 -0500

    Final-Project (90%)

--- js/index.js
+++ js/index.js
@@ -1,38 +1,40 @@
    
 $("nav a").on("click", function(){
-  var navId = $(this).attr("href");
-   $("html,body").animate({
-     scrollTop: $("#"+navId).offset().top
-   });
+	var navId = $(this).attr("href");
+	$("html,body").animate({
+		scrollTop: $("#"+navId).offset().top
+	});
 });
 
-// hover button
 
-$(document).ready(function () {
-    $(document).on('mouseenter', '.divbutton', function () {
-        $(this).find(":button").show();
-    }).on('mouseleave', '.divbutton', function () {
-        $(this).find(":button").hide();
-    });
-});
+
+
+
+// Toggle the quote
+
+// $('.tagline-main').on('click', function(){
+// 	$('#answer1').slideToggle(); 
+// });
 
 // youtube videos
 
-$('.play').click(function() {
- $('#video, #overlay').fadeIn('slow');
- $('#video-container').html('<iframe "width=560" "height=315" "src=https://www.youtube.com/embed/VzpP0KbjHU8" "frameborder=0 allowfullscreen></iframe>"');
-});
+// $('.diana_ross_image').on('click', function() {
+//  $('#video, #overlay').fadeIn('slow');
+//  $('#video-container').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/VzpP0KbjHU8" frameborder="0" allowfullscreen></iframe>');
+// });
 
-$(document).on('touchend, mouseup', function(e) {
-  if (!$('#video').is(e.target)) {
-    $('#video, #overlay').fadeOut('slow');
-    $('#video-container').html('');
-  }
-});
+// $(document).on('touchend, mouseup', function(e) {
+// 	if (!$('#video').is(e.target);
+// 		$('#video, #overlay').fadeOut('slow');
+// 		$('#video-container').html(''); 
+// 	}
+// });
 
-// $('#btn2').click(function() {
+// $("#artists").on("click" , function() {
 //   alert('video is not visible');
 // });
+
+// animate the width in javascript to cause css transition
 
 
 
